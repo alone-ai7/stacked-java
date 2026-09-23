6281d6a02c84e66eaeaba99b785adc2402a2396f
Elijah <elijahtahadlangit7@gmail.com>
2026-09-23 09:31:06 +0800

    Delete single_fie directory

--- single_fie/Print.java
+++
@@ -1,6 +0,0 @@
-public class Print{                // Note that the class should match the file name
-	public static void main(String[] args){
-		System.out.println("Hello World");  // basically "ln" is equivalent to "\n" in python
-		System.out.print("Hello Elijah");
-	}
-}
