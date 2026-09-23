97c880e8a965e4c8894fe94691d4b92381231360
Elijah <elijahtahadlangit7@gmail.com>
2026-09-23 10:40:43 +0800

    Add files via upload

---
+++ single_files/input.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;  
+
+public class input{
+	public static void main(String[] args){
+		
+		Scanner input = new Scanner(System.in); // "Scanner" is the type, "in" is the variable. DON'T CHANGE THE TYPE
+		
+		System.out.println("Welcome to my program");
+		System.out.print("What should we call you: ");
+		String your_name = input.nextLine();
+		System.out.printf("Hello, %s ", your_name); // printf = print formatted | %s = placeholder for the variable name
+	}
+}
+		
+		
